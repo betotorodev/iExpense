0afdb7ba796eccaa54f4d4bca73e8a279d88aeae
Beto Toro <beto.toro@zemoga.com>
2022-07-11 18:02:53 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Beto Toro on 11/07/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
